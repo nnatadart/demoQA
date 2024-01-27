0bf7507a7e6a1a1f035a9e52f8c2e40b1a2d76f4
Natalya <nnatadart@gmail.com>
2024-01-27 20:40:52 +0700

    ewew

--- src/test/java/junit/FirstJUTest.java
+++ src/test/java/junit/FirstJUTest.java
@@ -1,2 +1,24 @@
-package junit;public class FirstJUTest {
+package junit;
+
+import org.junit.jupiter.api.*;
+
+import java.lang.annotation.Target;
+
+public class FirstJUTest {
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("1");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("2");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("test body");
+        Assertions.assertTrue(3 > 2);
+    }
 }
